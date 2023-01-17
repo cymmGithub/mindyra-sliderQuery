a52eae8711a175db8633a0a4b756701bad1b411e
cymmgithub <przemswiercz@gmail.com>
2023-01-18 00:46:02 +0100

    cleanUp/linter formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import { AvatarSlider } from "./components/AvatarSlider/AvatarSlider";
 import { usersData } from "./db/usersData";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index element={<AvatarSlider userData={usersData} />} />
+    </Route>
+  )
+);
+
 export const App = () => {
-  return <AvatarSlider userData={usersData} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 };
