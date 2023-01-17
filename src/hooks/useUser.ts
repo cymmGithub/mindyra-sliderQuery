98cdcb6e96deb97a971c41953a0fc4e611cf7b0c
cymmgithub <przemswiercz@gmail.com>
2023-01-18 00:22:52 +0100

    cleanup/liter formatting

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,12 +1,6 @@
 import { fetcher } from "../utils/fetcher";
 import useSWR from "swr";
 
-interface ReqApiInfo {
-  avatarUrl: string;
-  isLoading: boolean;
-  isError: boolean;
-}
-
 export const useUser = (name: string) => {
   const { data, error, isLoading } = useSWR(
     `https://api.github.com/users/${name}`,
