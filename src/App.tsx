556b034db9c522adb45600c5545c20b8dfb968ee
cymm <przemswiercz@gmail.com>
2023-01-29 21:18:27 +0100

    Remove Query Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AvatarSlider } from "./components/AvatarSlider/AvatarSlider";
 import { usersData } from "./db/usersData";
 
@@ -9,7 +8,6 @@ export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <AvatarSlider userData={usersData} />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 };
